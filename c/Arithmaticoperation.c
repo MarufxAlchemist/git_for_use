22756476de39a0e56df46eae283d397adb4a94cb
Maruf <maruffnadaf@gmail.com>
2024-09-21 19:32:29 +0530

    Added C programs

---
+++ c/Arithmaticoperation.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+int main()
+{
+    int a=9 , b=4 , c ;
+    c = a+b ;
+    printf("Addition =%d\n",c);
+    c = a-b ;
+    printf("Substraction=%d\n",c);
+    c = a*b ;
+    printf("Multiplication=%d\n",c);
+    c= a/b;
+    printf("Division=%d\n",c);
+    c = a%b;
+    printf("Modulus=%d",c);
+    return 0;
+}
+
---
+++ c/README.md
@@ -0,0 +1,2 @@
+# C_Programming
+Basic Logical Programming in C
